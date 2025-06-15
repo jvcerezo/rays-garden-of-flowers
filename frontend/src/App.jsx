739e7d66b46682f461e61b42032fef5c332b7f18
jvcerezo <jvcerezo@up.edu.ph>
2025-06-16 00:38:47 +0800

    initialize Firebase in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { auth, db, storage } from './firebase/firebase';
+
 
 function App() {
   const [count, setCount] = useState(0)
